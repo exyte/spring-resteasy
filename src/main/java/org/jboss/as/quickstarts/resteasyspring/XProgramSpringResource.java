e15ca956304b1d25b0042e1d9a85d7718fa2be8f
Anton Salnik <skanton@gmail.com>
2019-11-12 20:15:58 +0700

    Add support for put, delete and get queries

--- src/main/java/org/jboss/as/quickstarts/resteasyspring/XProgramSpringResource.java
+++ src/main/java/org/jboss/as/quickstarts/resteasyspring/XProgramSpringResource.java
@@ -5,8 +5,12 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,11 +20,37 @@ public class XProgramSpringResource {
     @Autowired
     XProgramBean xProgramBean;
 
+    @GET
+    @Path("xprogram")
+    @Produces("text/plain")
+    public Response getXProgram(@QueryParam("q") String query) {
+        String result = xProgramBean.process(query);
+        return Response.ok(result).build();
+    }
+
     @POST
     @Path("xprogram")
     @Consumes("text/plain")
     @Produces("text/plain")
     public Response postXProgram(String body) {
+        String result = xProgramBean.process(body);
+        return Response.ok(result).build();
+    }
+
+    @PUT
+    @Path("xprogram")
+    @Consumes("text/plain")
+    @Produces("text/plain")
+    public Response putXProgram(String body) {
+        String result = xProgramBean.process(body);
+        return Response.ok(result).build();
+    }
+
+    @DELETE
+    @Path("xprogram")
+    @Consumes("text/plain")
+    @Produces("text/plain")
+    public Response deleteXProgram(String body) {
         String result = xProgramBean.process(body);
         return Response.ok(result).build();
     }
